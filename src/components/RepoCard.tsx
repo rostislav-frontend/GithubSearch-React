c913e8cc115c4aa973ea468c2b97362930618c38
rostislav <rostislav.plotnikov.04@mail.ru>
2022-08-13 22:57:28 +0300

    fix button repocard

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -29,19 +29,19 @@ export const RepoCard = ({ repo }: { repo: IRepo }) => {
         <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
             <a href={repo.html_url} target="_blank">
                 <h2 className="text-lg font-bold">{repo.full_name}</h2>
-                <p className="text-sm">
+                <p className="text-sm mt-2">
                     Forks: <span className="font-bold mr-2">{repo.forks}</span>
                     Watchers: <span className="font-bold">{repo.watchers}</span>
                 </p>
                 <p className="text-sm font-thin">{repo?.description}</p>
             </a>
             {!isFav ? <button
-                className="py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all"
+                className="py-2 px-10 bg-green-400 text-white rounded hover:shadow-md transition-all mt-2"
                 onClick={addToFavourite}
             >Add</button>
                 :
                 <button
-                    className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
+                    className="py-2 px-10 bg-red-400 text-white rounded hover:shadow-md transition-all mt-2"
                     onClick={removeFromFavourite}
                 >Remove</button>}
         </div>
