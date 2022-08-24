bcf3b027529f143a51c2854d86036cbea5170e8f
rostislav <rostislav.plotnikov.04@mail.ru>
2022-08-24 22:39:17 +0300

    fix favourites page

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -5,14 +5,19 @@ export function FavouritesPage() {
 
   const { favourites } = useAppSelector(state => state.github)
   if (favourites.length === 0) return <p className='text-center'>No items.</p>
+  console.log(favourites);
 
   return (
-    <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+    <div className='flex flex-col items-center justify-center pt-10'>
+      <h2 className='mb-5 text-[32px] font-bold'>Сохраненные репозитории</h2>
       <ul className="list-none">
-        {favourites.map(f => (
-          <li key={f}>
-            <a href={f} target={'_blank'}>{f}</a>
-          </li>
+        {favourites.map((f, index) => (
+          <a href={f} target={'_blank'}>
+            <li key={f} className='flex gap-2 border p-[10px] mb-5'>
+              <span>{index + 1}</span>
+              <span>{f}</span>
+            </li>
+          </a>
         ))}
       </ul>
     </div>
